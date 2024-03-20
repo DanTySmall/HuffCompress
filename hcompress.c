31a34e137f376c7194a55b4c8e75382afd37d0a1
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-03-19 21:35:42 -0400

    Opening File

--- hcompress.c
+++ hcompress.c
@@ -4,5 +4,18 @@
 
 int main(int argc, char const *argv[]) {
   /* code */
+  // Read From a File
+
+  // If File specified, open it
+  // If not, use text.txt
+  FILE* fp;
+    if (argc < 2){
+      fp = fopen("text.txt", "r");
+    }else{
+      fp = fopen(argv[0], "r");
+    }
+
+  printf("File Contents: \n");
+
   return 0;
 }
