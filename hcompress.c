d559a7d898e8bb7d1df137feaec7df575a0f1cf2
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-03-21 11:00:20 -0400

    Created Heap Structure and Functions

--- hcompress.c
+++ hcompress.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 
+// TODO: Make Functions that free structures
+
 typedef struct node{
 
   char character;
@@ -12,13 +14,27 @@ typedef struct node{
 }Node;
 
 
-typedef struct ArrayList{
+typedef struct arrayList{
 
   Node* array;
   int size;
   int capacity;
 
 }ArrayList;
+
+typedef struct ArrayListRef{
+
+  Node** array;
+  int size;
+  int capacity;
+
+}ArrayListRef;
+
+typedef struct heap{
+
+  ArrayListRef* alr;
+
+}Heap;
 
 // ArrayList of Nodes
 ArrayList* createAL(){
@@ -32,7 +48,8 @@ ArrayList* createAL(){
   return new;
 }
 
-ArrayList* append(ArrayList* al, char character, int freq){
+// Add Character to ArrayList
+void append(ArrayList* al, char character, int freq){
 
 
   if(al -> size == al -> capacity){ // If theres no space, expand array
@@ -57,6 +74,104 @@ ArrayList* append(ArrayList* al, char character, int freq){
 
 }
 
+// Creates ArrayList of Nodes Refrence
+ArrayListRef* createALR(){
+
+  // Allocate Space
+  ArrayListRef* new = malloc(sizeof(ArrayListRef));
+  new -> array = calloc(4, sizeof(Node*));
+  new -> size = 0;
+  new -> capacity = 4;
+
+  return new;
+}
+
+// Adds Node to ArrayListRef
+void appendR(ArrayListRef* alr, Node* n){
+
+  if(alr -> size == alr -> capacity){ // If theres no space, expand array
+
+    Node** temp = realloc(alr -> array, 2 * alr->capacity * sizeof(Node*));
+    if (temp)
+    {
+
+     alr -> array = temp;
+     alr -> capacity *= 2;
+    }
+
+  }
+
+
+  // Add Node Pointer to array
+  alr -> array[alr -> size] = n;
+  alr -> size++;
+
+}
+
+// Creates a Heap
+Heap* createH(){
+
+  // Allocate Memory
+  Heap* h = malloc(sizeof(Heap));
+  h -> alr = createALR();
+
+  // Return Heap
+  return h;
+
+}
+
+void percolateUp(Heap* h, int index){
+
+  // NULL Check
+  if(!h || !index) return;
+
+  // Check if node's frequency is less than the node above it swap
+  int parent = (index - 1) /2;
+
+  if(h -> alr -> array[index]->freq < h -> alr -> array[parent]->freq)
+  {
+    // Swap
+    Node* temp = h -> alr -> array[index];
+    h -> alr -> array[index] = h -> alr -> array[parent];
+    h -> alr -> array[parent] = temp;
+
+  }else{
+    return;
+  }
+
+  percolateUp(h,parent);
+
+}
+
+// Inserts Node into Heap
+void insert(Heap* h, Node* n){
+
+  // Place Node at End of Heap
+  appendR(h -> alr, n);
+
+  // Percolate up
+  percolateUp(h, h -> alr -> size - 1);
+
+}
+
+// Heapifies an ArrayList
+Heap* heapify (ArrayList* al){
+
+  // Allocate Memory
+  Heap* h = createH();
+
+  // Place every node in heap
+  for(int position = 0; position < al -> size; position++){
+
+    insert(h, &al -> array[position]);
+
+  }
+
+  // Return Heap
+  return h;
+
+
+}
 
 
 
@@ -65,7 +180,7 @@ int main(int argc, char const *argv[]) {
   // Read From a File
 
   // If File specified, open it
-  // If not, use text.txt
+    // If not, use text.txt
   FILE* fp;
     if (argc < 2){
       fp = fopen("text.txt", "r");
@@ -75,8 +190,8 @@ int main(int argc, char const *argv[]) {
 
 
   // Print Contents of File and Create Freq Array
-  int aend = 1 >> 8; // Size of ascii
-  int* freq = calloc(1>>8, sizeof(int));
+  int aend = 1 << 8; // Size of ascii
+  int* freq = calloc(aend, sizeof(int));
   char c = getc(fp);
 
   printf("File Contents: \n");
@@ -90,8 +205,60 @@ int main(int argc, char const *argv[]) {
     c = getc(fp);
   }
 
-  // printf("\n\n");
+  printf("\n\n");
 
+
+  // // Print the frequencies
+  // for(int i = 0; i < aend; i++ ){
+  //   printf("%c : %d\n", (char) i, freq[i] );
+  // }
+
+
+
+  // Add Frequencies to the ArrayList
+  ArrayList* arrayL = createAL();
+  for(int i = 0; i < aend; i++ ){
+
+    // If frequency is not 0 add to AL
+    if (freq[i] == 0) continue;
+    append(arrayL, (char)i, freq[i]);
+
+  }
+
+  // // Print The Contents of the ArrayList
+  // printf("File Contents:\n");
+  // for (int position = 0; position < arrayL -> size; position++){
+  //
+  //   printf("%c : %d\n", arrayL-> array[position].character, arrayL-> array[position].freq);
+  //
+  //
+  // }
+  //
+  // printf("\n");
+
+   Heap* heap = heapify(arrayL);
+
+
+   // // Print Contents of the Heap
+   // printf("Heap Contents: \n");
+   // for(int i = 0; i < heap -> alr -> size; i++ ){
+   //
+   //  printf("%c : %d\n", heap -> alr -> array[i] ->character, heap -> alr -> array[i] ->freq);
+   //
+   //
+   // }
+
+
+  // Reopen file for parsing
+  fclose(fp);
+    if (argc < 2){
+      fp = fopen("text.txt", "r");
+    }else{
+      fp = fopen(argv[0], "r");
+    }
+
+
+  fclose(fp);
 
   return 0;
 }
