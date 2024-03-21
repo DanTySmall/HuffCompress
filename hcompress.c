5e6c2703c2a6ab4de3303131a9ef947698060388
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-03-20 21:06:22 -0400

    ArrayList Created Holds All The Nodes

--- hcompress.c
+++ hcompress.c
@@ -13,6 +13,50 @@ typedef struct node{
 
 }Node;
 
+
+typedef struct ArrayList{
+
+  Node* array;
+  int size;
+  int capacity;
+
+}ArrayList;
+
+// ArrayList of Nodes
+ArrayList* createAL(){
+
+  // Allocate Space
+  ArrayList* new = malloc(sizeof(ArrayList));
+  new -> array = calloc(4, sizeof(Node));
+  new -> size = 0;
+  new -> capacity = 4;
+
+  return new;
+}
+
+ArrayList* append(ArrayList* al, char character, int freq){
+
+
+  if(al -> size == al -> capacity){ // If theres no space, expand array
+
+    Node* temp = realloc(al -> array, 2 * al->capacity * sizeof(Node));
+    if (temp)
+    {
+
+     al -> array = temp;
+     al -> capacity *= 2;
+    }
+
+  }
+
+
+  // Add Node to array
+  al -> array[al -> size].character = character;
+  al -> array[al -> size].freq = freq;
+  al -> array[al -> size].left = NULL;
+  al -> array[al -> size].right = NULL;
+  al -> size++;
+
 }
 
 
