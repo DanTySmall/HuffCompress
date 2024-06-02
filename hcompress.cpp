a2addb309a13217a82d80095b4ef7498e1021aae
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-06-02 07:09:37 -0400

    Program Can read from files
    Program reads from text.txt

--- hcompress.cpp
+++ hcompress.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(){
 
   // Read File
+  fstream source;
+  source.open("text.txt", ios::in);
+  char message[100];
+
+  source >> message;
+
+  cout << "Printing File Contents";
+  cout << message;
   // Put Contents of File to a Frequency Array
   // Make a Heap to Hold Frequencies
   // Create Huffman Tree
