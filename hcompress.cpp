e7d64ee2574d75fed52ebbcf258795038b6929dd
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-06-02 06:18:25 -0400

    Program Template

--- hcompress.cpp
+++ hcompress.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main(){
 
-  cout << "Hello, World!\n";
+  // Read File
+  // Put Contents of File to a Frequency Array
+  // Make a Heap to Hold Frequencies
+  // Create Huffman Tree
+  // Compress Text
+  // Write Text To File
+  // Close All Files
+
+
+
+
+
   return 0;
 
 }
