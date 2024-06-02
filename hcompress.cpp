beed15e5943e5f8a371bd65df3eb950bf562febc
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-06-02 08:45:28 -0400

    Program Reads File and puts Contents into Array

--- hcompress.cpp
+++ hcompress.cpp
@@ -5,9 +5,6 @@ using namespace std;
 int main(int argc, char *argv[]){
 
   // Read File
-  fstream source;
-
-  // Source Handling
   FILE* fp;
   if (argc < 2){
     fp = fopen("text.txt", "r");
@@ -15,19 +12,43 @@ int main(int argc, char *argv[]){
     fp = fopen(argv[1], "r");
   }
 
+
+  // Create Array to Hold Character Frequencies
+  int total = 0;
+  int aend = 1 << 8;
+  int* freq = new int[aend];
+  std::fill(freq, freq + aend, 0);
+
   // Print Contents of File
   cout << "Printing File Contents\n";
   char c = getc(fp);
   while(c != EOF){
 
-    // Prints Character
+    // Prints Character and adds to Frequency Array
     cout << c;
+
+    if (c == '\0'){
+      c = getc(fp);
+      continue;
+    }
+
+    freq[(int)c]++;
+    total++;
     c = getc(fp);
+
 
   }
   fclose(fp);
 
+  // Print all characters in array
+  cout << "Character Frequencies\n";
+  for (int i = 0; i < aend; i++){
 
+    if (freq[i] != 0){
+      cout << (char)i << " : " << freq[i] << endl;
+    }
+
+  }
 
 
 
